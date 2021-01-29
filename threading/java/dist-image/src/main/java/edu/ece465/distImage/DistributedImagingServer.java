6eb7ff47d2769a6bc207e83851c0bef7e321499d
Rob Marano <maranoro@amazon.com>
2021-01-28 21:17:26 -0500

    Added scaffold for a distributed Java app with tooling.

---
+++ threading/java/dist-image/src/main/java/edu/ece465/distImage/DistributedImagingServer.java
@@ -0,0 +1,26 @@
+package edu.cooper.ece465.distImage;
+
+import java.net.*;
+import java.io.*;
+ 
+public class DistributedImagingServer {
+    public static void main(String[] args) throws IOException {
+ 
+    if (args.length != 1) {
+        System.err.println("Usage: java DistributedImagingServer <port number>");
+        System.exit(1);
+    }
+ 
+        int portNumber = Integer.parseInt(args[0]);
+        boolean listening = true;
+         
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) { 
+            while (listening) {
+                new DistributedImagingThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}

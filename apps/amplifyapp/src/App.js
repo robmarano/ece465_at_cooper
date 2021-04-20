c456f43d448120c024307a5ac0ea596588c662b2
Rob Marano <robmarano@gmail.com>
2021-04-19 21:18:11 -0400

    Updated amplifyapp/src/App.js

--- apps/amplifyapp/src/App.js
+++ apps/amplifyapp/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
